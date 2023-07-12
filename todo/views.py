09f25fae84d1200bcd4438781478e8b34fd98509
1F10220103 <s1F102201032@iniad.org>
2023-07-12 23:55:46 +0900

    二回目のコミット

--- todo/views.py
+++ todo/views.py
@@ -21,7 +21,7 @@ def index(request):
     }
     return render(request,'todo/index.html',context)
 
-def detail(requst,task_id):
+def detail(request,task_id):
     try:
         task=Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
